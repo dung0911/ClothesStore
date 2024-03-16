1d10b107878170c82e02626b59ef08d3cc82c306
dung0911 <trungdung123123123@gmail.com>
2024-03-16 09:45:05 +0700

    Remove CategoryRepository in category file

--- src/main/java/com/shoesstore/shoesstore/controller/CategoryController.java
+++ src/main/java/com/shoesstore/shoesstore/controller/CategoryController.java
@@ -1,17 +1,8 @@
 package com.shoesstore.shoesstore.controller;
 
-import com.shoesstore.shoesstore.entity.Categories;
-import com.shoesstore.shoesstore.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 @Controller
 public class CategoryController {
-    @Autowired
-    private CategoryRepository categoryRepository;
+
 }
